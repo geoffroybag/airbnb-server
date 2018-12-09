69d541bbf8309570c9eefe3962a088e16bf49057
Mathis Peingnez <mathis.peingnez@gmail.com>
2018-12-09 16:45:09 +0100

    dontknwo

--- routes/message-router.js
+++ routes/message-router.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const User = require("../models/user-model");
-const Message = require("../models/message-model");
+// const express = require("express");
+// const User = require("../models/user-model");
+// const Message = require("../models/message-model");
 
-const router = express.Router();
-const io = require("socket.io")(http);
+// const router = express.Router();
+// const io = require("socket.io")(http);
 
-//GET THE ID OF THE USER
+// //GET THE ID OF THE USER
 
-router.post("/chat", (req, res, next) => {
+// router.post("/chat", (req, res, next) => {
 
-  // id of the message
-  Message.findOne()
-  .then(messageDoc => {
-    res.json(messageDoc.data)
-  })
-  .catch(err => next(err));
-});
+//   // id of the message
+//   Message.findOne()
+//   .then(messageDoc => {
+//     res.json(messageDoc.data)
+//   })
+//   .catch(err => next(err));
+// });
 
-io.on("connection", function(socket){
-  console.log("A user connected")
-});
+// io.on("connection", function(socket){
+//   console.log("A user connected")
+// });
 
-module.exports = router;
+// module.exports = router;
