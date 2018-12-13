fb084737bf08fa5a798791cdc3083f28b80e3969
Laura Stromboni <laura.stromboni@live.fr>
2018-12-13 17:34:57 +0100

    profil pic

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.864751f24ea3dd00ae9746284d8d0695.js"
+  "/precache-manifest.b7841edb5b0a1738c4acbdb21811e29f.js"
 );
 
 workbox.clientsClaim();
